065dc45007a5acc62b471ac425aa902cb18f9023
Otac27 <angelserpa7@gmail.com>
2025-06-30 20:59:22 -0500

    init commit

---
+++ src/main/java/org/example/githubintellijliteratura/GithubIntellijLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubintellijliteratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntellijLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubIntellijLiteraturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/githubintellijliteratura/GithubIntellijLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubintellijliteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntellijLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
